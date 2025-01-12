5bfa58bbec3295f69d439d69fe2beb545ab98e87
Julia Taborek <106110658+juliataborek@users.noreply.github.com>
2025-01-12 13:08:43 +0100

    Update aplikacja.py

--- App/aplikacja.py
+++ App/aplikacja.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 # WCZYTANIE DANYCH
 # wczytanie danych z normami
-data=pd.read_csv(r"C:\Users\julia\Documents\informatyka i ekonometria\semestr 3\inzynieria oprogramowania\norms_clean.csv",
+data=pd.read_csv(r"App/data/norms_clean.csv",
                  sep=";")
 
 # wczytanie danych z powiazanymi badaniami oraz specjalistami
